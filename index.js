44795bef6e7e4826ee54c62076f5c6dcc8104001
liufei <feiliu@58coin.com>
2020-08-29 21:33:08 +0800

    AntProvider

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './app/App';
 import { Provider } from 'react-redux'
+import { Provider as AntProvider } from '@ant-design/react-native'
 import store from './app/store'
 
 class Index extends Component {
     render() {
         return (
             <Provider store={store}>
-                <App />
+                <AntProvider>
+                    <App />
+                </AntProvider>
             </Provider>
         );
     }
